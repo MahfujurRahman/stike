27cd7e3d9fc287c79d88115a535967187c0b835c
Mahfujur Rahman <mahfujur705@gmail.com>
2018-07-20 00:12:36 +0600

    samething

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,33 @@
         $('[data-toggle="tooltip"]').tooltip();
 
 
+        $(".treger-menu").on("click", function(){
+            $(".hide-menu, .menu-overlay").addClass("active");
+            return false;
+        });
+
+        $("span.menu-close, .menu-overlay").on("click", function(){
+            $(".hide-menu, .menu-overlay").removeClass("active");
+        });
+
+        $("tooltip").tooltip();
+
+        $('.popover').popover({
+            container: 'body'
+          });
+
+          $(".counter-number").counterUp({
+            delay: 10,
+            time: 1000
+        });
+        $('#singel-list').waypoint(function() {
+            $(".counter-number span").css({ 
+                  opacity: "1",
+                  marginTop: "0"
+                });
+          }, { offset: 150 });
+
+
 
         
 
